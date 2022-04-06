3ef5111ecf88f9929c5133ffb93dbf4ce990197c
David Nunez merino <dnunez-m@car1s1.42barcelona.com>
2022-04-06 13:12:46 +0200

    una mas

--- ft_atoi.c
+++ ft_atoi.c
@@ -7,6 +7,26 @@
 int	ft_atoi(const char *str)
 {
 
+int i;
+int mult;
+int num;
 
+i = 0;
+mult = 1;
+num = 0;
 
+while(str[i] == ' ' || str[i] == 't' || str[i] == 'n' || str[i] == 'v' || str[i] == 'r')
+	i++;
+if(str[i] == '+')
+	i++;
+if(str[i] == '-')
+	mult = mult * -1;
+while(str[i] && (str[i] >= '0' && str[i] <= '9'))
+{
+	num = num * 10;
+	num = num + (str[i] - '0');
+	i++;
+}
+num = num * mult;
+return(num);
 }
---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int ft_strlen(char *str)
+{
+int i;
+
+i = 0;
+while(str[i])
+	i++;
+return (i); 
+}
+
+char    *ft_strdup(char *src)
+{
+
+int i;
+char *dest;
+
+i = 0;
+dest = ((char *)malloc(sizeof(char) * (ft_strlen(src) + 1)));
+if(!dest)
+	return(NULL);
+while(src[i])
+{
+	dest[i] = src[i];
+	i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
