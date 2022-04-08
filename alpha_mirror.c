2c75b5994dfd626bdbafc2ad520c5bda5fc7b2a8
David Nunez merino <dnunez-m@car1s1.42barcelona.com>
2022-04-08 11:54:45 +0200

    alpha_mirror

--- alpha_mirror.c
+++ alpha_mirror.c
@@ -1,6 +1,8 @@
+#include <unistd.h>
+
 int main(int argc, char **argv)
 {
-	inti	;
+	int	i;
 
 	i = 0;
 	if(argc != 2)
@@ -10,10 +12,20 @@ int main(int argc, char **argv)
 	}
 	while(argv[1][i])
 	{
-		if(argv[1][i] == 'a' || argv[1][i] == 'A' )
-			argv[1][i] +=  25;
-		else if(argv[1][i] == 'z' || argv[1][i] == 'Z')
-			argv[1][i] -+ 25;
-		else if(argv[i][i])
+		if(argv[1][i] >= 'a' && argv[1][i] <= 'z' )
+		{
+			argv[1][i] = 219 - argv[1][i];
+			write(1, &argv[1][i], 1);
+		}
+		else if(argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+		{
+			argv[1][i] = 155 - argv[1][i];
+			write(1, &argv[1][i], 1);
+		}
+		else
+			write(1, &argv[1][i], 1);
+		i++;
 	}
+	write(1, "\n", 1);
+	return(0);
 }
